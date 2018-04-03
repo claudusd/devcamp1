119b66cbd61bc37802b43e7a943d9111062f51cb
Pierre Tachoire <pierre@wisembly.com>
2018-04-03 20:51:55 +0200

    fix import int

--- import/src/ISFInsert.php
+++ import/src/ISFInsert.php
@@ -41,17 +41,18 @@ class ISFInsert
          */
 
         $req = $this->pdo->prepare("
-          INSERT INTO isf (year, region, department, insee, city, people, tax_avg, weatlh_avg) 
+          INSERT INTO isf (year, region, department, insee, city, people, tax_avg, weatlh_avg)
           VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
         $insee = str_replace(' ', '', $data[2]);
         $req->bindParam(1, $this->year, \PDO::PARAM_INT);
         $req->bindParam(2, $data[0]);
         $req->bindParam(3, $data[1]);
         $req->bindParam(4, $insee);
         $req->bindParam(5, $data[3]);
-        $req->bindParam(6, intval($data[4]), \PDO::PARAM_INT);
-        $req->bindParam(7, intval($data[5]), \PDO::PARAM_INT);
-        $req->bindParam(8, intval($data[6]), \PDO::PARAM_INT);
+        $req->bindParam(6, intval(preg_replace("/\W/", '', $data[4])), \PDO::PARAM_INT);
+        $req->bindParam(7, intval(preg_replace("/\W/", '', $data[6])), \PDO::PARAM_INT);
+        $req->bindParam(8, intval(preg_replace("/\W/", '', $data[5])), \PDO::PARAM_INT);
 
         $req->execute();
     }
